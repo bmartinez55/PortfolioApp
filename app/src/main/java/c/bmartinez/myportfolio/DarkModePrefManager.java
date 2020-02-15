9d46c77c8536225773912951ea2bbd931b5bcaa4
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-02-14 17:41:27 -0800

    Implementend DarkModePrefManager constructor

--- app/src/main/java/c/bmartinez/myportfolio/DarkModePrefManager.java
+++ app/src/main/java/c/bmartinez/myportfolio/DarkModePrefManager.java
@@ -15,4 +15,11 @@ public class DarkModePrefManager {
     private static final String PREF_NAME = "dark-mode";
     private static  final String IS_NIGHT_MODE = "IsNightMode";
 
+    public DarkModePrefManager(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME,PRIVATE_MODDE);
+        editor = pref.edit();
+    }
+
+    
 }
