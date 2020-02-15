7e12b57d10b715cc5aac6682a689299f51178d6b
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-02-14 16:42:13 -0800

    Implemented onNestPreScroll function

--- app/src/main/java/c/bmartinez/myportfolio/BottomNavigationBehavior.java
+++ app/src/main/java/c/bmartinez/myportfolio/BottomNavigationBehavior.java
@@ -5,6 +5,7 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +17,15 @@ public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomN
 
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, BottomNavigationView child, View dependency){
-        
+        boolean dependsOn = dependency instanceof FrameLayout;
+        return dependsOn;
+    }
+
+    @Override
+    public boolean onNestedPreScroll(CoordinatorLayout coordinatorLayout, BottomNavigationView child, View target, int dx, int dy, int[] consumed){
+        if(dy < 0)
+            showBottomNavigationView(child);
+        else if (dy > 0)
+            hideBottomNavigationView(child);
     }
 }
